1cd2c13776041496f8451200f31a60eb4b49b153
savage-floppy <treasure363@protonmail.com>
2021-05-13 02:22:52 +0530

    Added Profile model in authentication and tested

--- app/models.py
+++ app/models.py
@@ -5,7 +5,3 @@ from django.contrib.auth.models import User, AbstractUser
 
 # Create your models here.
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    address = models.CharField(max_length=256, blank=True)
-    phone = models.IntegerField()
--- authentication/models.py
+++ authentication/models.py
@@ -4,7 +4,15 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
-class UserProfile(models.Model):
+class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    address = models.CharField(max_length=500, blank=True)
+    address = models.CharField(max_length=100, blank=True)
+    city = models.CharField(max_length=100, blank=True)
+    state = models.CharField(max_length=100, blank=True)
+    zip_code = models.IntegerField(blank=True)
     phone = models.IntegerField(blank=True)
+    total_skipTraces = models.IntegerField(blank=True)
+    total_orders = models.IntegerField(blank=True)
+    total_spent = models.FloatField(blank=True)
+    pending_skipTraces = models.IntegerField(blank=True)
+    available_skipTraces = models.IntegerField(blank=True)
